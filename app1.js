4a6e5aae331a3442d3daea70f332fe4aaaaf6688
sakshamlohiya <72407458+sakshamlohiya@users.noreply.github.com>
2021-07-13 17:12:06 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -564,11 +564,8 @@ io.on('connection', socket => {
 
 
 
-const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
-server.listen(PORT, LOCAL_ADDRESS, () => {
-  const address = server.address();
-  console.log('server listening at', address);
-});
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
 
 
 
